2593b93153d3ee407b6fc8e548b14d6b39505dda
Ando <and.m97987@gmail.com>
2023-01-29 00:41:04 +0400

    JS example dictionaries key value Loops if else statements

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 // console.warn("This is warning");
 // console.error("This is error");
 
-console.log(5 + 10);
-console.log(5 * 10);
+// console.log(5 + 10);
+// console.log(5 * 10);
 
 //Working with strings
 // console.log("Hello Word");
@@ -27,13 +27,10 @@ console.log(5 * 10);
 //  console.log(myText);
 
 //Working with Booleans
-/*
-let myBool1 = true;
-let myBol2 = false;
-let isLogedIn = false;
-console.log(5<6);
-*/
-
+// let myBool1 = true;
+// let myBol2 = false;
+// let isLogedIn = false;
+// console.log(5<6);
 
 //Working with Nulls
 // let myNull = null;
@@ -43,22 +40,136 @@ console.log(5<6);
 // console.log(typeof(myText));
 
 //Working with Arrays
+// let countries = ["Armenia","China","Russia","USA","France","Germany"];
+// console.log(countries);
+// console.log(countries[2]);
+// countries[2] ="Armenia";
+// console.log(countries[2]);
+// countries.push("Georgia");
+// console.log(countries);
+// countries.unshift("Spain");
+// console.log(countries);
+// let lastElement = countries.pop();
+// console.log(lastElement);
+// console.log(countries);
+// console.log(countries.indexOf("France"));
+// console.log(countries.length);
+//
+// let people = [
+//   ["Andrew","John","Jacob","Rob"],
+//   ["Juline","Nicole","Jane","Monica"]
+// ];
+// console.log(people[0]);
+// console.log(people[1]);
+// let fullArray = people[0].concat(people[1]);
+// console.log(fullArray)
+//
+// let years  = [2018,2019,2020,2021,2022,2023];
+// console.log(years);
+//
+// let mixedArray = ["Arman",2023,true,null,"Ando"];
+// console.log(mixedArray);
 
-let countries = ["Armenia","China","Russia","USA","France","Germany"];
-console.log(countries);
-console.log(countries[2]);
-countries[2] ="Armenia";
-console.log(countries[2]);
-countries.push("Georgia");
-console.log(countries);
-countries.unshift("Spain");
-console.log(countries);
-let lastElement = countries.pop();
-console.log(lastElement);
-console.log(countries);
+//Working with dictionaries key value
+// let companies = {
+//   microsoft: 1950,
+//   ibm: 1965,
+//   google: 1985,
+//   amazon: [2002, "Jeff Bexos", "USA"],
+//   facebook: {
+//     year: 2005,
+//     owner: "Mark Zuckerberg",
+//     country: "USA",
+//   }
+// }
+// console.log(companies);
+// console.log(companies.microsoft);
+// console.log(companies.facebook.owner);
+// console.log(companies.amazon[2]);
+// companies.google = 1988;
+// console.log(companies);
 
-let years  = [2018,2019,2020,2021,2022,2023];
-console.log(years);
+// Working with Loops cikl
+// let countries = ["Armenia", "China", "Russia", "USA", "France", "Germany"];
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+// for (let i = 0; i < countries.length; i++) {
+//   console.log(countries[i]);
+// }
+// for (const country of countries) {
+//   console.log(country);
+// }
+// let i = 0;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
 
-let mixedArray = ["Arman",2023,true,null,"Ando"];
-console.log(mixedArray);
+// Working with if else statements
+// if (5 > 6) {
+//   console.log("Correct");
+// } else {
+//   console.log("Wrong");
+// }
+// let i = 0;
+// while (i <= 10) {
+//   if (i == 5) {
+//     i++;
+//     continue;
+//   } else {
+//     console.log(i);
+//     i++;
+//   }
+// }
+// let i = 0;
+// while (i <= 10) {
+//   if (i == 3) {
+//     i++;
+//     break;
+//   } else {
+//     console.log(i);
+//     i++;
+//   }
+// }
+// let i = 0;
+// while (i <= 10) {
+//   if (i == 5) {
+//     i++;
+//     continue;
+//   } else if (i == 8) {
+//     console.log("The value is 8");
+//     i++;
+//   } else {
+//     console.log(i);
+//     i++;
+//   }
+// }
+// let i = 0;
+// while (i <= 10) {
+//   if (i == 5 || i == 3) {
+//     console.log("The value is 3 or 5");
+//   } else {
+//     console.log(i);
+//   }
+//   i++;
+// }
+// let i = 0;
+// while (i <= 10) {
+//   if (i > 3 && i < 8) {
+//     console.log("The value is greater 3 and less 8");
+//   } else {
+//     console.log(i);
+//   }
+//   i++;
+// }
+// let i = 0;
+// while (i <= 10) {
+//   if ((i > 3 && i < 8) && i == 5) {
+//     console.log("The value is greater 3 and less 8");
+//   } else {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
