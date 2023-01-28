8f6febf1f55c4dbea2ce85a071225db378fd5f36
Ando <and.m97987@gmail.com>
2023-01-28 21:35:56 +0400

    JS example array null boolean string

--- js/main.js
+++ js/main.js
@@ -1,1 +1,64 @@
-alert('barev hayer');
+// alert("Hello Code Tubes");
+// console.log("Hello code Tubes");
+// console.warn("This is warning");
+// console.error("This is error");
+
+console.log(5 + 10);
+console.log(5 * 10);
+
+//Working with strings
+// console.log("Hello Word");
+// console.log("Hello Word" + "From Arman"); // erku stringner gumarum e irar ev stexcum mi naaxadasutyun
+// console.log("Hello Word".toUpperCase()); // bolor tarer mecatar
+// console.log("Hello Word".toLowerCase()); // bolor tarer poqratar
+//
+// let myNumber1 = 5;
+// let myNumber2 = 6;
+// myNumber2 = 8;
+// console.log(myNumber1 + myNumber2);
+// const theYear = 2018;
+// console.log(theYear);
+//  let myName = "Arman";
+//  let myCountry = "Armenia"
+//
+//  let myText = `My name is ${myName} and I'm from ${myCountry}`;   // ` ` ${} karoxenq texti mej avelacnel mer
+//           // console.log("Hello Word" + "From Arman");            // tvac popoxakaner aranc gumarman nshani
+//                                                                   // poxarinum e ays kodin
+//  console.log(myText);
+
+//Working with Booleans
+/*
+let myBool1 = true;
+let myBol2 = false;
+let isLogedIn = false;
+console.log(5<6);
+*/
+
+
+//Working with Nulls
+// let myNull = null;
+// let myNumber = 15;
+// let myText = "Some Text here";
+// let myBool = true;
+// console.log(typeof(myText));
+
+//Working with Arrays
+
+let countries = ["Armenia","China","Russia","USA","France","Germany"];
+console.log(countries);
+console.log(countries[2]);
+countries[2] ="Armenia";
+console.log(countries[2]);
+countries.push("Georgia");
+console.log(countries);
+countries.unshift("Spain");
+console.log(countries);
+let lastElement = countries.pop();
+console.log(lastElement);
+console.log(countries);
+
+let years  = [2018,2019,2020,2021,2022,2023];
+console.log(years);
+
+let mixedArray = ["Arman",2023,true,null,"Ando"];
+console.log(mixedArray);
